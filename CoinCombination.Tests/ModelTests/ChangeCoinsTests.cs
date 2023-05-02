d560472bd2ff774586986afe1512cee4452339ae
ChrisRDavila <crossdavila@gmail.com>
2023-05-02 10:47:24 -0700

    get and set test 2 and 3 pass, will get and set change amount

--- CoinCombination.Tests/ModelTests/ChangeCoinsTests.cs
+++ CoinCombination.Tests/ModelTests/ChangeCoinsTests.cs
@@ -13,6 +13,31 @@ namespace CoinCombination.Tests
       Change newChange = new Change(97);
       Assert.AreEqual(typeof(Change), newChange.GetType()); 
     }
+
+    [TestMethod]
+    public void GetAmount_ReturnAmount_Int()
+    {
+      //Arrange
+      int testAmount = 97;
+      Change newChange = new Change(testAmount);
+      // Act
+      int result = newChange.Cents;
+      //Assert
+      Assert.AreEqual(testAmount, result);
+    }
+
+    [TestMethod]
+    public void SetAmount_SetValueOfAmount_Void()
+    {
+      //Arrange
+      Change newChange = new Change(75);
+      int newAmount = 75;
+      //Act
+      newChange.Cents = newAmount;
+      //Assert
+      Assert.AreEqual(newAmount, newChange.Cents);
+    }
+    
   }
 }
 
